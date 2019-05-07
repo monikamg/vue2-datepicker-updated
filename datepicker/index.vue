eb285cde3fd5a93c6cf5bbdc5bee0eb24a053ae8
Raja Syed Abuthahir J <rajasyedab@gmail.com>
2019-05-07 18:15:48 +0530

    Update index.vue

--- datepicker/index.vue
+++ datepicker/index.vue
@@ -172,7 +172,8 @@ export default {
       currentValue: this.value,
       position: null,
       userInput: null,
-      ranges: [] // 快捷选项
+      ranges: [] // 快捷选项,
+      computedMask: ''
     }
   },
   watch: {
